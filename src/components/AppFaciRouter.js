df9f13aa2ed2c9ae7cf43d42a2cace031adc628a
张烜久 <369786460@qq.com>
2017-06-08 17:28:52 +0800

    增加一个路由

--- src/components/AppFaciRouter.js
+++ src/components/AppFaciRouter.js
@@ -5,10 +5,12 @@ import {
   Link
 } from 'react-router-dom';
 import ContentContainer from './ContentContainer';
+import ManageWrapper from './manage/ManageWrapper';
 
 const router = () => (
   <Router>
     <Route exact path="/" component={ContentContainer}/>
+    <Route path="/manage" component={ManageWrapper}/>
   </Router>
 )
 
---
+++ src/components/manage/ManageWrapper.js
@@ -0,0 +1,27 @@
+import React, {
+	Component
+} from 'react';
+
+class ManageWrapper extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    };
+  }
+
+  componentDidMount(){
+    
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    
+  }
+
+	render() {
+		return (<div>
+      ManageWrapper!
+    </div>);
+	}
+}
+
+export default ManageWrapper;
